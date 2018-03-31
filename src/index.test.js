29fba7383898a98e963039b9c8a6bbc066254ff7
Emilio Astarita <emilio.astarita@gmail.com>
2018-03-31 14:15:09 -0300

    Adding jest tests: First test for init method.

---
+++ src/index.test.js
@@ -0,0 +1,19 @@
+import Amplitude from './index.js';
+
+test('Init method must be called with args', () => {
+
+    const initMethod = jest.fn();
+
+    window.amplitude = {
+        getInstance: () => ({
+            init: initMethod
+        }),
+    };
+
+    const options = {};
+    const callback = () => {};
+    Amplitude.init('key', 'uId', {}, () => {});
+
+    expect(initMethod).toHaveBeenCalledWith('key', 'uId', options, callback);
+
+});
