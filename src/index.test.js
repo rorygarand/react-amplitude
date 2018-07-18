5409f9f5d4382b148b865caaee0c18b55712138b
Rory Garand <github@rorygarand.com>
2018-07-18 12:28:51 -0500

    add basic tests

--- src/index.test.js
+++ src/index.test.js
@@ -7,7 +7,7 @@ let init;
 let isNewSession;
 let logEvent;
 let logEventWithTimestamp;
-let resetUserId;
+let regenerateDeviceId;
 let setOptOut;
 let setUserId;
 let setUserProperties;
@@ -21,7 +21,7 @@ describe('Amplitude', () => {
         isNewSession = jest.fn();
         logEvent = jest.fn();
         logEventWithTimestamp = jest.fn();
-        resetUserId = jest.fn();
+        regenerateDeviceId = jest.fn();
         setOptOut = jest.fn();
         setUserId = jest.fn();
         setUserProperties = jest.fn();
@@ -35,7 +35,7 @@ describe('Amplitude', () => {
                 isNewSession,
                 logEvent,
                 logEventWithTimestamp,
-                resetUserId,
+                regenerateDeviceId,
                 setOptOut,
                 setUserId,
                 setUserProperties
@@ -50,15 +50,99 @@ describe('Amplitude', () => {
         expect(init).toHaveBeenCalledWith('key', 'uId', options, callback);
     });
 
+    it('should call clearUserProperties', () => {
+        const options = {};
+        const callback = () => {};
+        Amplitude.init('key', 'uId', options, callback);
+        
+        Amplitude.clearUserProperties();
+        expect(clearUserProperties).toHaveBeenCalled();
+    });
+
+    it('should call getSessionId', () => {
+        const options = {};
+        const callback = () => {};
+        Amplitude.init('key', 'uId', options, callback);
+        
+        Amplitude.getSessionId();
+        expect(getSessionId).toHaveBeenCalled();
+    });
+
+    it('should call identify with args', () => {
+        const options = {};
+        const callback = () => {};
+        Amplitude.init('key', 'uId', options, callback);
+
+        const idObj = {};
+        const callbackId = () => {};
+        Amplitude.identify(idObj, callbackId);
+        expect(identify).toHaveBeenCalledWith(idObj, callbackId);
+    });
+
+    it('should call isNewSession', () => {
+        const options = {};
+        const callback = () => {};
+        Amplitude.init('key', 'uId', options, callback);
+        
+        Amplitude.isNewSession();
+        expect(isNewSession).toHaveBeenCalled();
+    });
+
     it('it should call logEvent with args', () => {
         const options = {};
         const callback = () => {};
         Amplitude.init('key', 'uId', options, callback);
 
         const eventProperties = {};
-        const callbackEvent = () => {}
-        Amplitude.logEvent('event', eventProperties, callbackEvent)
+        const callbackEvent = () => {};
+        Amplitude.logEvent('event', eventProperties, callbackEvent);
         expect(logEvent).toHaveBeenCalledWith('event', eventProperties, callbackEvent);
+    });
+
+    it('it should call logEventWithTimestamp with args', () => {
+        const options = {};
+        const callback = () => {};
+        Amplitude.init('key', 'uId', options, callback);
+
+        const eventProperties = {};
+        const timestamp = 0;
+        const callbackEvent = () => {};
+        Amplitude.logEventWithTimestamp('event', eventProperties, timestamp, callbackEvent);
+        expect(logEventWithTimestamp).toHaveBeenCalledWith('event', eventProperties, timestamp, callbackEvent);
+    });
+
+    it('should call resetUserId', () => {
+        const options = {};
+        const callback = () => {};
+        Amplitude.init('key', 'uId', options, callback);
+        
+        Amplitude.resetUserId();
+        expect(regenerateDeviceId).toHaveBeenCalled();
+        expect(setUserId).toHaveBeenCalled();
+    });
+
+    it('it should call setUserProperties with args', () => {
+        const options = {};
+        const callback = () => {};
+        Amplitude.init('key', 'uId', options, callback);
+
+        const userProperties = {};
+        Amplitude.setUserProperties(userProperties);
+        expect(setUserProperties).toHaveBeenCalledWith(userProperties);
+    });
+
+    it('it should call setOptOut with args', () => {
+        const options = {};
+        const callback = () => {};
+        Amplitude.init('key', 'uId', options, callback);
+
+        let enable = true;
+        Amplitude.setOptOut(enable);
+        expect(setOptOut).toHaveBeenCalledWith(enable);
+
+        enable = false;
+        Amplitude.setOptOut(enable);
+        expect(setOptOut).toHaveBeenCalledWith(enable);
     });
 
     it('deprecated methods should call new methods', () => {
@@ -67,8 +151,8 @@ describe('Amplitude', () => {
         Amplitude.initialize('key', 'uId', options, callback);
 
         const eventProperties = {};
-        const callbackEvent = () => {}
-        Amplitude.event('event', eventProperties, callbackEvent)
+        const callbackEvent = () => {};
+        Amplitude.event('event', eventProperties, callbackEvent);
         expect(logEvent).toHaveBeenCalledWith('event', eventProperties, callbackEvent);
     });
 });
